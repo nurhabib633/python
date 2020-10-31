e144c4485671a7d10b906bd6a8bdd6afc2fd0325
nurhabib633 <nurhabib633@gmail.com>
2020-10-31 18:33:36 +0600

    large number

---
+++ large number.py
@@ -0,0 +1,15 @@
+def large():
+    a=float(input("Enter the number"))
+    b=float(input("Enter the number"))
+    c=float(input("Enter the number"))
+    if(a>c)&(a>c):
+        l=a
+    elif(b>a)&(b>c):
+        l=b
+    else:
+        l=c
+    print(l)
+        
+large()
+    
+    
